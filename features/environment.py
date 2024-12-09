57adda83a19fb6879b77338bf62383a0826ec004
Sebastian Hernandez <sebastianh034@gmail.com>
2024-12-09 14:52:06 -0700

    3

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
-    context.driver = webdriver.Chrome(ChromeDriverManager().install())
-    context.driver.maximize_window()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    service = Service(ChromeDriverManager().install())
+    context.driver = webdriver.Chrome(service=service, options=chrome_options)
 
 def after_all(context):
     context.driver.quit()
